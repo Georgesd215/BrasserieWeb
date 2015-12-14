9b3ab50c8dc2ba81127ffa153a1731b0c25b48c1
Diab <georges.diab@etu.unistra.fr>
2015-12-14 22:45:58 +0100

    registration

--- BrasserieSite/BrasserieSite/urls.py
+++ BrasserieSite/BrasserieSite/urls.py
@@ -8,13 +8,19 @@ urlpatterns = patterns('',
     # Examples:
     url(r'^$', "BrasserieSite.views.index", name='index'),
     url(r'^NosBiere/', "BrasserieSite.views.NosBiere", name='NosBiere'),
-    url(r'^contact/$', "BrasserieSite.views.contact", name='contact'),
+    url(r'^accounts/profile/', "BrasserieSite.views.profile", name='profile'),
+    url(r'^BARBANE_DEMENTHE/$', "BrasserieSite.views.BARBANE_DEMENTHE", name='BARBANE_DEMENTHE'),
+    url(r'^BARBANE_FLORALE/$', "BrasserieSite.views.BARBANE_FLORALE", name='BARBANE_FLORALE'),
+    url(r'^BARBANE_BLONDE/$', "BrasserieSite.views.BARBANE_BLONDE", name='BARBANE_BLONDE'),
+    url(r'^BARBANE_NOEL/$', "BrasserieSite.views.BARBANE_NOEL", name='BARBANE_NOEL'),
+    url(r'^BARBANE_MONDIALE/$', "BrasserieSite.views.BARBANE_MONDIALE", name='BARBANE_MONDIALE'),
+    url(r'^BARBANE_DANS_LES_VIGNES/$', "BrasserieSite.views.BARBANE_DANS_LES_VIGNES", name='BARBANE_DANS_LES_VIGNES'),
         
     # url(r'^BrasserieSite/', include('BrasserieSite.foo.urls')),
     
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    url(r'^accounts/', include('registration.backends.default.urls')),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 
--- BrasserieSite/BrasserieSite/views.py
+++ BrasserieSite/BrasserieSite/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-from .forms import ContactForm
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
 def index(request):
     return render_to_response('index.html', {},RequestContext(request))
 
@@ -10,10 +9,28 @@ def NosBiere(request):
     return render_to_response('NosBiere.html', {},RequestContext(request))
 
 
-def contact(request):
-    form_class = ContactForm
-    
-    return render(request, 'contact.html', {
-        'form': form_class,
-    })
+def BARBANE_DEMENTHE(request):
+    return render_to_response('BARBANE_DEMENTHE.html', {},RequestContext(request))
 
+def profile(request):
+    return render_to_response('registration/profile.html', {},RequestContext(request))
+
+def BARBANE_FLORALE(request):
+    return render_to_response('BARBANE_FLORALE.html', {},RequestContext(request))
+
+def BARBANE_BLONDE(request):
+    return render_to_response('BARBANE_BLONDE.html', {},RequestContext(request))
+
+def BARBANE_NOEL(request):
+    return render_to_response('BARBANE_NOEL.html', {},RequestContext(request))
+
+def BARBANE_MONDIALE(request):
+    return render_to_response('BARBANE_MONDIALE.html', {},RequestContext(request))
+
+def BARBANE_DANS_LES_VIGNES(request):
+    return render_to_response('BARBANE_DANS_LES_VIGNES.html', {},RequestContext(request))
+
+@login_required
+def Commande(request):
+    return render(request, "commande.html", {})
+  
